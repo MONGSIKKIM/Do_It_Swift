7dec527c1e44461d0a865376600943fbb61b4baf
hongsiik <rkghk8545@naver.com>
2023-12-18 23:32:50 +0900

    chapter4 mission clear

--- practice/Chapter4/DatePicker/DatePicker/ViewController.swift
+++ practice/Chapter4/DatePicker/DatePicker/ViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     let timeSelector : Selector = #selector(ViewController.updateTime) //함수 지정
+//    let alramSelector : Selector = #selector(ViewController.alarmTime) //알람 함수 지정
     let interval = 1.0
     var count = 0
+    var alarmTime : String!
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblPickerTime: UILabel!
     
@@ -26,8 +28,12 @@ class ViewController: UIViewController {
         let datePickerView = sender
         
         let formatter = DateFormatter()
+        let formatter_alarm = DateFormatter()
+        
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        formatter_alarm.dateFormat = "hh:mm:aaa"
         lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        alarmTime = formatter_alarm.string(from: datePickerView.date)
     }
     
     @objc func updateTime() {
@@ -36,8 +42,25 @@ class ViewController: UIViewController {
         let date = NSDate()
         
         let formatter = DateFormatter()
+        let formatter_alarm = DateFormatter()
+        
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        formatter_alarm.dateFormat = "hh:mm:aaa"
+        
         lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        let currentTime = formatter_alarm.string(for: date)
+        
+        if (alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        }
+        else {
+            view.backgroundColor = UIColor.white
+        }
     }
+//    @objc func alarmTime() {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "hh:mm:aaa"
+//        
+//    }
 }
 
